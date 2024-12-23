bd9b2d5dbff11d796c685f37e806ffea03237651
Saurabh sadhwani <sumit.sadhwani56@gmail.com>
2024-12-24 01:21:39 +0530

    updated alerts

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     context={
         'request':'request'
     }
+    messages.success(request,'this is test message')
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
@@ -20,8 +22,9 @@ def contact(request):
         email=request.POST.get('email')
         phone=request.POST.get('phone')
         desc=request.POST.get('desc')
-        contact=Contact(name='name',email='email',phone='phone',desc='desc',date=datetime.today())
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request,"Form submitted !!")
     return render(request,'contact.html')
     # return HttpResponse("This is contact page")
 def services(request):
